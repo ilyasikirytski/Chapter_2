a9afe6633754bb258bbe4f5c1ef8016a199b7230
123 <bandidos-ice@mail.ru>
2021-12-24 13:49:59 +0300

    Kot_B_2 - Made TODO task and formatting the console print

--- src/Kot_B_2.kt
+++ src/Kot_B_2.kt
@@ -3,28 +3,28 @@
 /**
  * TODO то же что и в B_2
  */
-fun main(){
+fun main() {
     println("Введите одну цифру: и нажмите <Enter> :")
     val scan = readLine().orEmpty()
     val n = scan.toInt()
     println("Матрица N*N")
     for (i in 1..n) {
         for (k in 1..n) {
-            System.out.printf("%4d", i * k)
+            print("\t${i * k}")
         }
         println()
     }
     println("По очерёдно - слева направо")
     for (i in 0 until n) {
         for (k in 0 until n) {
-            System.out.printf("%4d", k + n * i + 1)
+            print("\t${k + n * i + 1}")
         }
         println()
     }
     println("По очерёдно - сверху вниз")
     for (k in 0 until n) {
         for (i in 0 until n) {
-            System.out.printf("%4d", k + n * i + 1)
+            print("\t${k + n * i + 1}")
         }
         println()
     }
