f3cae92ec3a0ab5c1b9adcb057ec02d0c109a42c
123 <bandidos-ice@mail.ru>
2021-12-24 13:30:41 +0300

    Kot_B_2 - Made TODO task

--- src/Kot_B_2.kt
+++ src/Kot_B_2.kt
@@ -7,10 +7,25 @@ fun main(){
     println("Введите одну цифру: и нажмите <Enter> :")
     val scan = readLine().orEmpty()
     val n = scan.toInt()
+    println("Матрица N*N")
     for (i in 1..n) {
         for (k in 1..n) {
-            System.out.printf("%4d ", i * k)
+            System.out.printf("%4d", i * k)
         }
-        println("")
+        println()
+    }
+    println("По очерёдно - слева направо")
+    for (i in 0 until n) {
+        for (k in 0 until n) {
+            System.out.printf("%4d", k + n * i + 1)
+        }
+        println()
+    }
+    println("По очерёдно - сверху вниз")
+    for (k in 0 until n) {
+        for (i in 0 until n) {
+            System.out.printf("%4d", k + n * i + 1)
+        }
+        println()
     }
 }
